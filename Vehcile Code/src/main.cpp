e4a807185af8d528ad148aaa89c0ef20db22aad1
Jasper Cusiel <jaspercusiel@icloud.com>
2022-08-30 17:52:34 +1200

    Add LoRa functionality and power button logic

--- Vehcile Code/src/main.cpp
+++ Vehcile Code/src/main.cpp
@@ -7,8 +7,8 @@
 #include <SPI.h>
 
 // RP2040 Board
-int SDA = 6;
-int SCL = 7;
+int Board_SDA = 6;
+int Board_SCL = 7;
 
 // MAX17048 Battery Fuel Gauge
 SFE_MAX1704X lipo(MAX1704X_MAX17048); 
@@ -32,8 +32,8 @@ MS5637 barometricSensor;
 
 // GPS
 int SPI_CLK = 2;
-int MOSI = 3;
-int MISO = 4;
+int GPS_MOSI = 3;
+int GPS_MISO = 4;
 int GPS_CS = 5;
 int TX = 0;
 int RX = 1;
@@ -43,15 +43,15 @@ int INT = 17;
 int AN = 26;
 
 // LoRa 
-const int RADIO_FREQ_MHZ = 915;
-const int RFM_CS = 1;
-const int RFM_RST = 1;
-const int RFM_IQR = 1;
-byte localAddress = 0xAA;
-byte destinationAddress = 0xBB;
+const int RFM_CS = 1;             // RFM95 chip select pin
+const int RFM_RST = 1;            // RFM95 reset pin
+const int RFM_IQR = 1;            // RFM95 IQR pin
+byte localAddress = 0xAA;         // address of this device
+byte destinationAddress = 0xBB;   // destination to send to
+byte msgCount = 0;                // count of out going messages
 int count = 0;
-long lastSendTime = 0;
-int interval = 200;
+long lastSendTime = 0;            // last send time
+int interval = 500;               //interval between sends
 
 // Micro SD Card
 int SDO = 12;
@@ -61,6 +61,85 @@ int DATA_1 = 11;
 int DATA_2 = 10;
 int DATA_3 = 9;
 
+// Soft power switch functions
+void fastPowerDown()
+{
+  pinMode(POWER_BUTTON, OUTPUT);
+  digitalWrite(POWER_BUTTON, LOW);
+  pinMode(FAST_OFF, OUTPUT);
+  digitalWrite(FAST_OFF, LOW);
+  powerPressedStartTime = millis();
+  while (1)
+  {
+    delay(1);
+  }
+}
+
+void slowPowerDown()
+{
+  pinMode(POWER_BUTTON, OUTPUT);
+  digitalWrite(POWER_BUTTON, LOW);
+  powerPressedStartTime = millis();
+  Serial.print("Pulling power line low");
+  while (1)
+  {
+    delay(1);
+  }
+}
+
+// LoRa functions
+
+void sendMessage(String outgoing)
+{
+  LoRa.beginPacket();             // start packet
+  LoRa.write(destinationAddress); // add sender address
+  LoRa.write(localAddress);       // add sender address
+  LoRa.write(msgCount);           // add message ID
+  LoRa.write(outgoing.length());  // add payload length
+  LoRa.print(outgoing);           // add payload
+  LoRa.endPacket();
+  msgCount++;                     // increment message ID
+}
+
+void receiveMessage(int packetSize)
+{
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available())
+  {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length())    // check length for error
+  {
+    Serial.println("Error: Message length does not match length");
+    return;
+  }
+
+  if (recipient != localAddress)              // check message is for this device
+  {
+    Serial.println("Error: Recipient adress does not match local address");
+    return;
+  }
+  
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
+}
 
 void setup(void) {
   //Power Switch Input Setup
@@ -91,14 +170,14 @@ void setup(void) {
   Serial.println("Begin");
 
   //I2C Initialization
-  Wire1.setSDA(SDA);
-  Wire1.setSCL(SCL);
+  Wire1.setSDA(Board_SDA);
+  Wire1.setSCL(Board_SCL);
   Wire1.begin();
   barometricSensor.begin(Wire1);
 
   // LoRa Initialization
   LoRa.setPins(RFM_CS, RFM_RST, RFM_IQR);
-  if (!LoRa.begin(RADIO_FREQ_MHZ))
+  if (!LoRa.begin(915E6))
   {
     Serial.println("LoRa init failed, check connections.");
     while (1){}
@@ -116,6 +195,44 @@ void setup(void) {
 }
 
 void loop(void) {
+  while (digitalRead(POWER_BUTTON) == LOW)
+  {
+    if (digitalRead(POWER_BUTTON) == LOW && powerPressedStartTime == 0)
+    {
+      //Debounce check
+      delay(debounceDelay);
+      if (digitalRead(POWER_BUTTON) == LOW)
+      {
+        Serial.println("User is pressing power button. Start timer.");
+        powerPressedStartTime = millis();
+      }
+    }
+    else if (digitalRead(POWER_BUTTON) == LOW && powerPressedStartTime > 0)
+    {
+      //Debounce check
+      delay(debounceDelay);
+      if (digitalRead(POWER_BUTTON) == LOW)
+      {
+        if ((millis() - powerPressedStartTime) > 2000)
+        {
+          Serial.println("Time to power down!");
+          fastPowerDown();
+        }
+      }
+    }
+    else if (digitalRead(POWER_BUTTON) == HIGH && powerPressedStartTime > 0)
+    {
+      //Debounce check
+      delay(debounceDelay);
+      if (digitalRead(POWER_BUTTON) == HIGH)
+      {
+        Serial.print("Power button released after ms: ");
+        Serial.println(millis() - powerPressedStartTime);
+      }
+      powerPressedStartTime = 0; //Reset var to return to normal 'on' state
+    }
+  }
+  
   // Check sensor data
   if (barometricSensor.isConnected())
   {
@@ -148,60 +265,5 @@ void loop(void) {
     interval = random(2000) + 1000;
   }
 
-
-
-}
-
-void fastPowerDown()
-{
-  pinMode(POWER_BUTTON, OUTPUT);
-  digitalWrite(POWER_BUTTON, LOW);
-  pinMode(FAST_OFF, OUTPUT);
-  digitalWrite(FAST_OFF, LOW);
-  powerPressedStartTime = millis();
-  while (1)
-  {
-    delay(1);
-  }
-}
-
-void sendMessage(String outgoing)
-{
-  LoRa.beginPacket();
-  LoRa.write(destinationAddress);
-  LoRa.write(localAddress);
-  LoRa.write(outgoing.length());
-  LoRa.print(outgoing);
-  LoRa.endPacket();
-}
-
-void receiveMessage(int packetSize)
-{
-  if (packetSize == 0) return;
-  int recipient = LoRa.read();
-  byte sender = LoRa.read();
-  byte incomingLength = LoRa.read();
-
-  String incoming = "";
-
-  while (LoRa.available())
-  {
-    incoming += (char)LoRa.read();
-  }
-  // Error checking
-  if (incomingLength != incoming.length())
-  {
-    Serial.println("Error: Message length does not match length");
-    return;
-  }
-
-  if (recipient != localAddress)
-  {
-    Serial.println("Error: Recipient adress does not match local address");
-    return;
-  }
-  
-  Serial.print("Received data" + incoming);
-  Serial.print(" from 0x" + String(sender, HEX));
-  Serial.println(" to 0x" + String(recipient, HEX));
+  receiveMessage(LoRa.parsePacket());
 }
